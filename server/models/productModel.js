5719682fd22bd6f6ddef4011a246dc713c6f21f2
AShr12xz <122085103+AShr12xz@users.noreply.github.com>
2024-08-23 18:31:24 +0530

    Two models added

---
+++ server/models/productModel.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required']
+    },
+    quantityAvailable: {
+        type: Number,
+        required: [true, 'Quantity is required']
+    },
+    description: {
+        type: String,
+        required: [true, 'Description is required']
+    },
+    council: {
+        type: String,
+        required: [true, 'Category is required']
+    },
+    managedBy: {
+        type: String,
+        required: [true, 'Managed by is required'],
+        lowercase: true,
+        validate: [validator.isEmail, 'Please provide a valid email']
+    },
+    image: {
+        type: String,
+        required: [true, 'Image is required'],
+        validate: [validator.isURL, 'Please provide a valid URL']
+    }
+});
+
+const Product = mongoose.model('Product', productSchema);
+module.exports = Product;
---
+++ server/models/userProductModel.js
@@ -0,0 +1,72 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userProductSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required']
+    },
+    quantityIssued: {
+        type: Number,
+        required: [true, 'Quantity is required']
+    },
+    issuerName: {
+        type: String,
+        required: [true, 'Issuer is required']
+    },
+    issuerMail: {
+        type: String,
+        required: [true, 'Issuer mail is required'],
+        lowercase: true,
+        validate: [validator.isEmail, 'Please provide a valid email']
+    },
+    requestDate: {
+        type: Date,
+        required: [true, 'Request date is required']
+    },
+    issuedDate: {
+        type: Date,
+        required: [true, 'Issued date is required']
+    },
+    returnDate: {
+        type: Date,
+        required: [true, 'Return date is required']
+    },
+    secyApprovalStatus: {
+        type: String,
+        enum: [
+            "Approved",
+            "Rejected",
+            "Pending"
+        ],
+        default: "Pending"
+    },
+    managerApprovalStatus: {
+        type: String,
+        enum: [
+            "Approved",
+            "Rejected",
+            "Pending"
+        ],
+        default: "Pending"
+    },
+    returnDescription: {
+        type: String,
+        required: [true, 'Description is required']
+    },
+    council: {
+        type: String,
+        required: [true, 'Category is required']
+    },
+    managedBy: {
+        type: String,
+        required: [true, 'Managed by is required'],
+        lowercase: true,
+        validate: [validator.isEmail, 'Please provide a valid email']
+    },
+    image: {
+        type: String,
+        required: [true, 'Image is required'],
+        validate: [validator.isURL, 'Please provide a valid URL']
+    }
+});
